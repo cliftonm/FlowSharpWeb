06f96b5e7d43f9057697855753a9775d0ce69f74
cliftonm <marc.clifton@gmail.com>
2018-06-06 18:53:42 -0400

    Added keyed listener to support detaching a listener by key.

--- event.js
+++ event.js
@@ -1,14 +1,28 @@
 ﻿class Event {
     constructor() {
         this.listeners = [];
+        this.keyedListeners = {};
     }
 
+    // Attaches a listener for the lifetime of the object containing the event.
+    // These listeners cannot be detached.
     attach(listener) {
         this.listeners.push(listener);
+    }
+
+    // Attaches a listener with a specific key, which can be detached
+    attachKeyed(key, listener) {
+        this.keyedListeners[key] = listener;
+    }
+
+    // Detaches the listener with the specified key.
+    detachKeyed(key) {
+        delete this.keyedListeners[key];
     }
 
     // args (if any) is expected to be a dictionary of key-value pairs.
     fire(sender, args) {
         this.listeners.forEach(listener => listener(sender, args));
+        Object.values(this.keyedListeners).forEach(listener => listener(sender, args));
     }
 }
--- views/propertyGridView.js
+++ views/propertyGridView.js
@@ -1,20 +1,24 @@
 ﻿class PropertyGridView {
     constructor(mouseController) {
         this.mouseController = mouseController;
+        this.currentModel = undefined;
         mouseController.eventShapeSelected.attach(this.onShapeSelected.bind(this));
     }
 
     // Show the shape ID on the property grid.
     onShapeSelected(sender, args) {
         document.getElementById(Constants.SHAPE_ID).innerHTML = args.shapeId;
+        this.unregisterExistingPropertyChangedEvent(args.model);
+        this.registerPropertyChangedEvent(args.model);
     }
 
-    //             console.log("Entering " + shapeController.model.shapeId);
-
-    propertyChanged(model, propertyName, value) {
+    propertyChanged(sender, args) {
+        let model = sender;
+        let propertyName = args.propertyName;
+        let value = args.value;
 
         if (model.shapeName !== undefined && value != null) {
-            console.log(model.shapeName + " : " + propertyName + ' = ' + value);
+            // console.log(model.shapeName + " : " + propertyName + ' = ' + value);
 
             // TODO: x, y, width, height and translate all affect the x, y, w, h PG controls.
             // Also, we map to a function that deals with the setting of the value, particularly
@@ -26,5 +30,16 @@
                 document.getElementById(gridControlId).value = value;
             }
         }
+    }
+
+    unregisterExistingPropertyChangedEvent(model) {
+        if (this.currentModel !== undefined && this.currentModel != model) {
+            this.currentModel.eventPropertyChanged.detachKeyed("pgv", this.propertyChanged.bind(this));
+        }
+    }
+
+    registerPropertyChangedEvent(model) {
+        this.currentModel = model;
+        this.currentModel.eventPropertyChanged.attachKeyed("pgv", this.propertyChanged.bind(this));
     }
 }
